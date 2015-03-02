9fc51fe8bfbedb884b15337baa93599369760994
Bodegi <Bodegigaming@gmail.com>
2015-03-01 22:37:37 -0500

    Started Project
    Built UI, some basic classes. Started the recursive folder hunting.

---
+++ Excel_Extractor/Excel_Extractor/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using Extraction;
+
+namespace Excel_Extractor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private void btnBrowseIn_Click(object sender, RoutedEventArgs e)
+        {
+            var open = new System.Windows.Forms.FolderBrowserDialog();
+            var fileInteraction = open.ShowDialog();
+            switch (fileInteraction)
+            {
+                case System.Windows.Forms.DialogResult.OK:
+                    string path = open.SelectedPath;
+                    txtDir.Text = path;
+                    break;
+                case System.Windows.Forms.DialogResult.Cancel:
+                default:
+                    txtDir.Text = "Select Directory to Search";
+                    break;
+            }
+
+        }
+
+        private void btnBrowseOut_Click(object sender, RoutedEventArgs e)
+        {
+            var open = new System.Windows.Forms.FolderBrowserDialog();
+            var fileInteraction = open.ShowDialog();
+            switch (fileInteraction)
+            {
+                case System.Windows.Forms.DialogResult.OK:
+                    string path = open.SelectedPath;
+                    txtOut.Text = path;
+                    break;
+                case System.Windows.Forms.DialogResult.Cancel:
+                default:
+                    txtOut.Text = "Select Location for Output";
+                    break;
+            }
+
+        }
+
+        private void btnExtract_Click(object sender, RoutedEventArgs e)
+        {
+            if(txtDir.Text != "Select Directory to Search")
+            {
+                if(txtOut.Text != "Select Location for Output")
+                {
+                    new FileSearch(txtDir.Text, txtOut.Text);
+                }
+                else
+                {
+                    MessageBox.Show("Please select a location to output the final spreadsheet", "Error");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a directory to search", "Error");
+            }
+        }
+
+
+    }
+}
---
+++ Excel_Extractor/Extraction/FileSearch.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Extraction
+{
+    public class FileSearch
+    {
+        public FileSearch(string dir, string output)
+        {
+            Excel final = new Excel();
+            List<string> lstFilesFound = new List<string>();
+            bool isExcel = false;
+            foreach(string d in Directory.GetDirectories(dir))
+            {
+                foreach(string f in Directory.GetFiles(d))
+                {
+                    isExcel = extension(f);
+                    if(isExcel == true)
+                    {
+
+                    }
+                }
+            }
+        }
+
+        private bool extension(string file)
+        {
+            string ext = "";
+            int count = 0;
+            count = file.IndexOf(".");
+            ext = file.Substring(count).ToUpper();
+            if (ext == ".XLS")
+            {
+                return true;
+            }
+            if (ext == ".XLSX")
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
